75b75fb41f036c3232a48413ff5b031892bfe0ea
eduarger <eduarger123@gmail.com>
2016-07-16 12:42:12 -0500

    Adición mas parámetros
    - búsqueda en maxbins
    - búsqueda maxdepth
    - eliminación del 1Maximo

--- src/main/scala/analisis.scala
+++ src/main/scala/analisis.scala
@@ -76,15 +76,22 @@ def saveRedTable(dataFrameIn: DataFrame, nam: String) : Unit  = {
      Logger.getLogger("org").setLevel(Level.WARN)
      Logger.getLogger("hive").setLevel(Level.WARN)
      logger.info("Solicitando recursos a Spark")
-     val conf = new SparkConf().setAppName("AnalisisP2P")
+     val conf = new SparkConf().setAppName("AnalisisP2P").set("spark.executor.memory","7373m")
      val sc = new SparkContext(conf)
      val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
-     // getting the parameters
+     logger.info("........getting the parameters...............")
+     // 
      val numPartitions=args(1).toInt
-     val numOfTrees= args(3).toInt
-     val arrayNTres=args.slice(4,4+numOfTrees)
-     var trees = arrayNTres.map(_.toInt)
-     val maxDepth=args(2).toInt
+     val numMaxDepth=args(2).toInt
+     val arrayNDepth=args.slice(3,3+numMaxDepth).map(_.toInt)
+     val numOfTrees= args(3+numMaxDepth).toInt
+     val trees=args.slice(3+numMaxDepth+1,3+numMaxDepth+numOfTrees+1).map(_.toInt)
+     val numOfBins= args(3+numMaxDepth+numOfTrees+1).toInt
+     val arrayBins=args.slice(3+numMaxDepth+numOfTrees+2,3+numMaxDepth+numOfTrees+numOfBins+1).map(_.toInt)
+     
+     println("Depth: " + arrayNDepth)
+     println("trees: " + trees)
+     println("Bins: " + arrayBins)
      if (args(0)=="y")
      {
      logger.info("........The Original database will be reading and saving...............")
@@ -97,82 +104,98 @@ def saveRedTable(dataFrameIn: DataFrame, nam: String) : Unit  = {
      logger.info("........Reading datosFinalesRed...............")
      var data = sqlContext.sql("SELECT * FROM datosFinalesRed where fraude==1 OR fraude==-1").coalesce(numPartitions)
      val names = data.columns
-     val ignore = Array("idsesion", "fraude")
+     val ignore = Array("idsesion", "fraude", "1_maximo")
      val assembler = (new VectorAssembler()
      .setInputCols( for (i <- names if !(ignore contains i )) yield i)
      .setOutputCol("features"))
      logger.info("........Converting to features...............")
      data = assembler.transform(data)
-          
+     //data.write.mode(SaveMode.Overwrite).saveAsTable("labeledDatos")
+    // var data = sqlContext.sql("SELECT * FROM labeledDatos where fraude==1 OR fraude==-1").coalesce(numPartitions)
+   
      
      logger.info("..........Conviertinedo DF a labeling...............")
      val rows: RDD[Row] = data.rdd
      val labeledPoints: RDD[LabeledPoint]=rows.map(row =>{LabeledPoint(row.getInt(25).toDouble,
      row.getAs[SparseVector](39))})
-    import sqlContext.implicits._
-    val labeledDF=labeledPoints.toDF()
+     import sqlContext.implicits._
+     val labeledDF=labeledPoints.toDF().coalesce(numPartitions)
     //labeledDF.write.mode(SaveMode.Overwrite).saveAsTable("labeledDatos")
         
      
-     val labelIndexer = new StringIndexer()
+     val labelIndexer = (new StringIndexer()
      .setInputCol("label")
      .setOutputCol("indexedLabel")
-     .fit(labeledDF)
+     .fit(labeledDF))
          
-     val featureIndexer = new VectorIndexer()
+     val featureIndexer = (new VectorIndexer()
     .setInputCol("features")
     .setOutputCol("indexedFeatures")
-    .setMaxCategories(4)
-    .fit(labeledDF)
+    .setMaxCategories(2)
+    .fit(labeledDF))
     
     var textOut="tipo,trees,tp,fn,tn,fp,TPR,SPC,PPV,ACC,F1 \n"
 	var textOut2=""
-    var textOut3="tress \n"
+    var textOut3="trees \n"
     
     for (x <- trees) {
     
- 
-   val nTrees=x  
    
+   val nTrees=x  
+  
+  // for( a <- 1 to k){
    logger.info("..........inicinando ciclo con un valor de trees..............."+ nTrees)
    // Split the data into training and test sets (30% held out for testing)
    val Array(trainingData, testData) = labeledDF.randomSplit(Array(0.7, 0.3))
 
    // Train a RandomForest model.
-   val rf = new RandomForestClassifier()
+  val rf = (new RandomForestClassifier()
   .setLabelCol("indexedLabel")
   .setFeaturesCol("indexedFeatures")
-  .setNumTrees(nTrees)
-  .setMaxDepth(maxDepth)
+  .setNumTrees(nTrees))
+  
 
    // Convert indexed labels back to original labels.
-   val labelConverter = new IndexToString()
+  val labelConverter = (new IndexToString()
   .setInputCol("prediction")
   .setOutputCol("predictedLabel")
-  .setLabels(labelIndexer.labels)
+  .setLabels(labelIndexer.labels))
 
 // Chain indexers and forest in a Pipeline
-val pipeline = new Pipeline()
-  .setStages(Array(labelIndexer, featureIndexer, rf, labelConverter))
+val pipeline = (new Pipeline()
+  .setStages(Array(labelIndexer, featureIndexer, rf, labelConverter)))
 // creating the cross validation without paramter grid
-val paramGrid = new ParamGridBuilder().build()
-val cv = new CrossValidator()
+// val paramGrid = new ParamGridBuilder().build()
+val paramGrid = (new ParamGridBuilder()
+  .addGrid(rf.maxDepth, arrayNDepth)
+  .addGrid(rf.impurity, Array("entropy", "gini"))
+  .addGrid(rf.maxBins, arrayBins)
+  .build())
+  
+val cv = (new CrossValidator()
   .setEstimator(pipeline)
   .setEvaluator(new BinaryClassificationEvaluator)
   .setEstimatorParamMaps(paramGrid)
-  .setNumFolds(5) // Use 3+ in practice 
+  .setNumFolds(5))
 
 logger.info("..........Training...............")
 // Train model.  This also runs the indexers.
 val model = cv.fit(trainingData)
 // writing the importances
 val rfModel = model.bestModel.asInstanceOf[PipelineModel].stages(2).asInstanceOf[RandomForestClassificationModel]
+//val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
 val importances=rfModel.featureImportances
+//val parametros=rfModel.
 textOut2=(textOut + nTrees + " " + importances + "\n" )
 println(textOut2)
 
+
+// best parameters
+val par = cv.getEstimatorParamMaps.zip(model.avgMetrics)
 //saving the best model
-textOut3=textOut3 + "---Learned classification forest model" + nTrees+ " ---\n" + rfModel.toDebugString + "\n\n"
+textOut3=textOut3 + "---Learned classification forest model" + nTrees+ " ---\n" + par.toString + "\n" + rfModel.toDebugString + "\n\n"
+
+
 
 
 logger.info("..........Testing...............")
@@ -218,9 +241,12 @@ textOut=(textOut + "train," +  nTrees + ","+ tp + "," + fn + "," + tn + "," + fp
 PPV + "," + acc + "," + f1  + "\n" )
 println(textOut)
 
+//}
+
 }
 
-// witing the files
+logger.info("..........writing the files...............")
+
 val pw = new PrintWriter(new File("Out.txt" ))
 pw.write(textOut)
 pw.close
@@ -246,9 +272,19 @@ pw3.close
 /*Aqui!!!!!!!!!!!!!!!!!!!!!!
 
 
-spark-submit --class "analisis" AnalisisP2P-assembly-1.0.jar n 150 7 6 10 30 50 80 100 150
+spark-submit --class "analisis" AnalisisP2P-assembly-1.0.jar n 200 7 5 6 10 30 50 80 100 150
 spark-submit --class "analisis" AnalisisP2P-assembly-1.0.jar [y=calculo, n=toma tabla]
  [numpartitions] [numero de valores de arboles] [arrya con numero de arboles]
+                                                              0 1   2  3 4  5  6  7  8  9 10 11  12 13 14 15
+ spark-submit --class "analisis" AnalisisP2P-assembly-1.0.jar n 200 3 10 15 25 5 10 25 50 80 100 3  32 64 128 
+ 
+ val numPartitions=args(1).toInt
+ val numMaxDepth=args(2).toInt
+ val arrayNDepth=args.slice(3,3+numMaxDepth).map(_.toInt)
+ val numOfTrees= args(3+numMaxDepth).toInt
+ val trees=args.slice(3+numMaxDepth+1,3+numMaxDepth+numOfTrees+1).map(_.toInt)
+ val numOfBins= args(3+numMaxDepth+numOfTrees+1).toInt
+ val arrayBins=args.slice(3+numMaxDepth+numOfTrees+2,3+numMaxDepth+numOfTrees+numOfBins+1).map(_.toInt)
 
 
 logger.info("..........Comienza PCA...............")
